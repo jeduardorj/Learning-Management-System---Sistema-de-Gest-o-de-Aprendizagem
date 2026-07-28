bb18f5546739918a4fc2dc7a10a22b01fbacfb77
jeduardorj <zeduardo.aguiar@gmail.com>
2026-07-28 18:53:20 -0300

    feat(dashboard): adiciona dashboard admin e student com indicadores agregados

---
+++ src/LMS.Application/DTOs/Dashboard/AdminDashboardDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.Application.DTOs.Dashboard;
+
+public class AdminDashboardDto
+{
+    public int TotalStudents { get; set; }
+    public int TotalCourses { get; set; }
+    public int TotalActiveCourses { get; set; }
+    public int TotalEnrollments { get; set; }
+    public int TotalCertificatesIssued { get; set; }
+    public decimal AverageCompletionRate { get; set; }
+    public IEnumerable<PopularCourseDto> MostPopularCourses { get; set; } = [];
+}
---
+++ src/LMS.Application/Interfaces/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using LMS.Application.DTOs.Dashboard;
+
+namespace LMS.Application.Interfaces;
+
+public interface IDashboardService
+{
+    Task<AdminDashboardDto> GetAdminDashboardAsync();
+    Task<StudentDashboardDto> GetStudentDashboardAsync();
+}
